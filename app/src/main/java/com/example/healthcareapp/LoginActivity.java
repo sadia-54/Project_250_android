2febd9a646f39ab099eb2509b5633588e80c7a87
jessia <sadiajessia@gmail.com>
2024-01-09 13:55:44 +0600

    message "login successful"

--- app/src/main/java/com/example/healthcareapp/LoginActivity.java
+++ app/src/main/java/com/example/healthcareapp/LoginActivity.java
@@ -3,12 +3,34 @@ package com.example.healthcareapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edUsername, edPassword;
+    Button btn;
+    TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edUsername = findViewById(R.id.editTextLoginUsername);
+        edPassword = findViewById(R.id.editTextLoginPassword);
+        btn = findViewById(R.id.buttonLogin);
+        tv = findViewById(R.id.textViewNewUser);
+
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view){
+                Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
