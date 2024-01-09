87fd2bedbd43e6323b1e14da130492defbbbb9a0
jessia <sadiajessia@gmail.com>
2024-01-09 23:19:40 +0600

    logical constraints of reg page

--- app/src/main/java/com/example/healthcareapp/RegisterActivity.java
+++ app/src/main/java/com/example/healthcareapp/RegisterActivity.java
@@ -2,13 +2,89 @@ package com.example.healthcareapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText edUsername, edPassword, edConfirmPass, edEmail;
+    Button btn;
+    TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edUsername = findViewById(R.id.editTextRegUsername);
+        edEmail = findViewById(R.id.editTextRegEmail);
+        edPassword = findViewById(R.id.editTextLoginPassword);
+        edConfirmPass = findViewById(R.id.editTextRegConfirmPassword);
+        btn = findViewById(R.id.buttonRegister);
+        tv = findViewById(R.id.textViewExistingUser);
+
+        //to go to the login page
+        tv.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                String email = edEmail.getText().toString();
+                String confirmpassword = edConfirmPass.getText().toString();
+                if (username.length() == 0 || password.length() == 0 || email.length() == 0 || confirmpassword.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Please fill all details", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (password.compareTo(confirmpassword) == 0) {
+                        if(isvalid(password)){
+                            Toast.makeText(getApplicationContext(), "Record inserted", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Password must contain at least 8 characters having letters, digits and symbols", Toast.LENGTH_SHORT).show();
+                        }
+                    } else
+                        Toast.makeText(getApplicationContext(), "Password and confirm password should be same", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+        public static boolean isvalid(String passwordhere){
+            int f1=0, f2=0, f3=0;
+            if(passwordhere.length()<8){
+                return false;
+            }else{
+                for(int p=0; p<passwordhere.length(); p++){
+                    if(Character.isLetter(passwordhere.charAt(p))){
+                        f1=1;
+                    }
+                }
+                for(int r=0; r<passwordhere.length(); r++){
+                    if(Character.isDigit(passwordhere.charAt(r))){
+                        f2 = 1;
+                    }
+                }
+                for(int s=0; s<passwordhere.length(); s++){
+                    char c = passwordhere.charAt(s);
+                    if(c>=33 && c<=46 || c==64){
+                        f3=1;
+                    }
+                }
+                if(f1==1 && f2==1 && f3==1) return true;
+                return false;
+            }
+
+        }
+
 }
