a2a0c19e6e67a5c8616eae0dbfede18b4a283741
jessia <sadiajessia@gmail.com>
2024-01-09 22:28:11 +0600

    Registration Page

--- app/src/main/java/com/example/healthcareapp/LoginActivity.java
+++ app/src/main/java/com/example/healthcareapp/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.healthcareapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,7 +30,23 @@ public class LoginActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View view){
+                // to check the username or password is empty or not
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                if(username.length()==0 || password.length()==0){
+                    Toast.makeText(getApplicationContext(), "Please fill all details", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+
                 Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // for new user
+        tv.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
     }
