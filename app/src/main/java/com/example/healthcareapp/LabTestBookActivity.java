73c3c10aa05c8f02eb9131b9f45dd8c2217627d4
jessia <sadiajessia@gmail.com>
2024-01-19 23:38:45 +0600

    lab test book activity

---
+++ app/src/main/java/com/example/healthcareapp/LabTestBookActivity.java
@@ -0,0 +1,48 @@
+package com.example.healthcareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LabTestBookActivity extends AppCompatActivity {
+    EditText edName, edAddress, edContact, edPinCode;
+    Button btnBook;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab_test_book);
+
+        edName = findViewById(R.id.editTextLTBFullName);
+        edAddress = findViewById(R.id.editTextLTBAddress);
+        edContact = findViewById(R.id.editTextLTBContact);
+        edPinCode = findViewById(R.id.editTextLTBPin);
+        btnBook = findViewById(R.id.buttonLTBBook);
+
+        Intent intent = getIntent();
+        String[] price = intent.getStringExtra("price").toString().split(java.util.regex.Pattern.quote(":"));
+        String date = intent.getStringExtra("date");
+        String time = intent.getStringExtra("time");
+
+        btnBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedpreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+                String username = sharedpreferences.getString("username", "").toString();
+
+                Database db = new Database(getApplicationContext(), "healthcare", null, 1);
+                db.addOrder(username, edName.getText().toString(), edAddress.getText().toString(), edContact.getText().toString(), Integer.parseInt(edPinCode.getText().toString()), date.toString(), time.toString(), Float.parseFloat(price[1].toString()), "lab");
+                db.removeCart(username, "lab");
+                Toast.makeText(getApplicationContext(),"Your booking is done successfully", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(LabTestBookActivity.this, HomeActivity.class));
+            }
+        });
+    }
+}
