5a53514b6d16902e35de4522eced11674eebc05d
jessia <sadiajessia@gmail.com>
2024-01-19 23:39:22 +0600

    updated database

--- app/src/main/java/com/example/healthcareapp/Database.java
+++ app/src/main/java/com/example/healthcareapp/Database.java
@@ -8,6 +8,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+
 public class Database extends SQLiteOpenHelper {
 
     public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version){
@@ -20,6 +22,9 @@ public class Database extends SQLiteOpenHelper {
 
         String qry2 = "create table cart(username text, product text, price float, otype text)";
         sqLiteDatabase.execSQL(qry2);
+
+        String qry3 = "create table orderplace(username text, fullname text, address text, contactno text, pincode int, date text, time text, amount float, otype text)";
+        sqLiteDatabase.execSQL(qry3);
 
     }
 
@@ -97,6 +102,40 @@ public class Database extends SQLiteOpenHelper {
         str[1]=otype;
         SQLiteDatabase db = getWritableDatabase();
         db.delete("cart", "username=? and otype=?", str);
+        db.close();
+    }
+
+    public ArrayList getCartData(String username, String otype){
+        ArrayList<String> arr = new ArrayList<>();
+        SQLiteDatabase db = getReadableDatabase();
+        String str[] = new String[2];
+        str[0] = username;
+        str[1] = otype;
+        Cursor c = db.rawQuery("select * from cart where username = ? and otype = ?", str);
+        if(c.moveToFirst()){
+            do{
+                String product = c.getString(1);
+                String price = c.getString(2);
+                arr.add(product+"$"+price);
+            }while(c.moveToNext());
+        }
+        db.close();
+        return arr;
+    }
+
+    public void addOrder(String username, String fullname, String address, String contact, int pincode, String date, String time, float price, String otype){
+        ContentValues cv = new ContentValues();
+        cv.put("username", username);
+        cv.put("fullname", fullname);
+        cv.put("address", address);
+        cv.put("contactno", contact);
+        cv.put("pincode", pincode);
+        cv.put("date", date);
+        cv.put("time", time);
+        cv.put("amount", price);
+        cv.put("otype", otype);
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert("orderplace", null, cv);
         db.close();
     }
 }
